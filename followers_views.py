d4d5cead08166e7af08e40aecdc92b24c99d960c
michaelsoto0786 <89877935+michaelsoto0786@users.noreply.github.com>
2022-02-20 21:51:25 -0500

    update

---
+++ followers_views.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.rcParams["figure.figsize"] = [7.50, 3.50]
+plt.rcParams["figure.autolayout"] = True
+
+headers = ['Name', 'Age', 'Marks']
+
+df = pd.read_csv('followers_views.py', names=headers)
+
+df.set_index('Name').plot()
+
+plt.show()
