0c7e7888c98218b09d404730fceaaa699da96913
michaelsoto0786 <89877935+michaelsoto0786@users.noreply.github.com>
2022-03-07 08:41:10 -0500

    update

---
+++ pie graph.py
@@ -0,0 +1,13 @@
+#Three lines to make our compiler able to draw:
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([35, 25, 25, 15])
+
+plt.pie(y)
+plt.show() 
+
