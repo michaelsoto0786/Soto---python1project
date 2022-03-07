a2dfeb4244cf266fc579b44785d4d90bfba357bf
michaelsoto0786 <89877935+michaelsoto0786@users.noreply.github.com>
2022-03-07 18:09:14 -0500

    update

--- Bar graph.py
+++ Bar graph.py
@@ -4,14 +4,14 @@ import pandas as pd
 plt.style.use('bmh')
 df = pd.read_csv('Instagram_Data.csv')
 
-x = df['User uuid']
+x = df['User ID']
 y = df['Likes']
 
 
 
 
 
-plt.xlabel('User uuid', fontsize=18)
+plt.xlabel('User ID', fontsize=18)
 plt.ylabel('Likes', fontsize=16)
 plt.bar(x, y)
 
---
+++ print show csv.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv('Instagram_Data.csv')
+
+print(df.to_string())
